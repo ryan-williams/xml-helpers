059db2cda8b8bbf9f5b90477f4ebf4ba08a2f839
Ryan Williams <ryan@runsascoded.com>
2023-04-21 16:01:12 -0400

    make-toc.py improvements

--- make-toc.py
+++ make-toc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import re
 from re import match
 
 import click
@@ -9,8 +9,9 @@ RGX = '(?P<level>#{2,}) (?P<title>.*) <a id="(?P<id>[^"]+)"></a>'
 
 
 @click.command()
+@click.option('-i', '--indent-size', type=int, default=4, help="Indent size (spaces)")
 @click.argument('file')
-def main(file):
+def main(indent_size, file):
     with open(file, 'r') as f:
         for line in f.readlines():
             line = line.rstrip('\n')
@@ -18,7 +19,8 @@ def main(file):
             if not m:
                 continue
             level, title, id = len(m['level']), m['title'], m['id']
-            indent = '  ' * (level - 2)
+            indent = ' ' * (indent_size * (level - 2))
+            title = re.sub(r'\[([^]]+)](?:\([^)]+\))?', r'\1', title)
             print(f'{indent}- [{title}](#{id})')
 
 
